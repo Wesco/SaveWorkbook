95024db1a2de2742e08d90b60b7e8a81a81e49ba
TReische <treische@wesco.com>
2013-04-10 09:06:30 -0400

    Added save ds and bo 117 by ISN

--- SaveWorkbook/ThisAddIn.cs
+++ SaveWorkbook/ThisAddIn.cs
@@ -45,7 +45,7 @@ namespace SaveWorkbook
 
             string path;
             string fileName;
-            string reportType = "";
+            string[] reportType = new string[2];
             string branch = "";
             int ISN = 0;
 
@@ -54,23 +54,22 @@ namespace SaveWorkbook
             {
                 branch = (ActiveSheet.Range["A3"].Value).ToString();
 
-                reportType = (ActiveSheet.Range["A1"].Value).ToString();
-                reportType = reportType.Replace(" ", String.Empty);
-                reportType = reportType.Substring(reportType.Length - 10);
+                reportType[0] = (ActiveSheet.Range["A1"].Value).ToString();
+                reportType[1] = (ActiveSheet.Range["A1"].Value).ToString();
+
+
+                reportType[0] = reportType[0].Replace(" ", String.Empty);
+                reportType[0] = reportType[0].Substring(reportType[0].Length - 10);
+
+                reportType[1] = reportType[1].Replace(" ", String.Empty);
+                reportType[1] = reportType[1].Substring(reportType[1].Length - 19);
             }
 
             //If it is a back order report
-            if (reportType == "BACKORDERS")
+            if (reportType[0] == "BACKORDERS")
             {
                 //Try to find the inside sales number
-                for (int i = 1; i < ActiveSheet.UsedRange.Columns.Count; i++)
-                {
-                    if ((string)ActiveSheet.Cells[2, i].Value == "IN")
-                    {
-                        ISN = (int)ActiveSheet.Cells[3, i].Value;
-                        break;
-                    }
-                }
+                int.TryParse((ActiveSheet.Cells[3, FindColumnHeader(2, "IN")].Value).ToString(), out ISN);
 
                 if (ISN != 0)
                 {
@@ -92,10 +91,56 @@ namespace SaveWorkbook
                     }
                 }
             }
+            else if (reportType[1] == "DIRECTSHIPPEDORDERS")
+            {
+                //Try to find the inside sales number
+                int.TryParse((ActiveSheet.Cells[3, FindColumnHeader(2, "IN")].Value).ToString(), out ISN);
 
-            System.Windows.Forms.MessageBox.Show(reportType);
+                if (ISN != 0)
+                {
+                    path = @"\\br3615gaps\gaps\3615 117 Report\ByInsideSalesNumber\" + ISN + @"\";
+                    fileName = branch + " " + String.Format("{0:M-dd-yy}", dt) + " DSORDERS" + ".xlsx";
+
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    try
+                    {
+                        App.oApp.ActiveWorkbook.SaveAs(path + fileName, Excel.XlFileFormat.xlOpenXMLWorkbook);
+                    }
+                    catch (Exception e)
+                    {
+                        //If error is not due to user canceled save display the error message
+                        if (e.Message.ToLower() != "exception from hresult: 0x800a03ec")
+                            System.Windows.Forms.MessageBox.Show(e.Message.ToString());
+                        //System.Windows.Forms.MessageBox.Show("File could not be saved!");
+                    }
+                }
+
+            }
         }
 
+        /// <summary>
+        /// Finds the specified column and returns the column number.
+        /// If no column is found then 0 is returned.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private int FindColumnHeader(int row, string text)
+        {
+            for (int col = 1; col < ActiveSheet.UsedRange.Columns.Count; col++)
+            {
+                if ((ActiveSheet.Cells[row, col].Value).ToString() == text)
+                {
+                    return col;
+                }
+            }
+
+            return 0;
+        }
 
         #region AddIn_Events
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
