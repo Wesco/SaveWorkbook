5f24f068e3811d3534ebe7e21b9f1e432455de39
TReische <treische@wesco.com>
2013-05-17 15:14:43 -0400

    Restructured settings

--- SaveWorkbook/Settings.cs
+++ SaveWorkbook/Settings.cs
@@ -44,10 +44,13 @@ namespace SaveWorkbook
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            if (IsValidPath(txt117Path.Text) &
-                IsValidPath(txt473Path.Text) &
-                IsValidPath(txtGapsPath.Text))
+            if (IsValidPath(txt117Path.Text) & IsValidPath(txt473Path.Text) & IsValidPath(txtGapsPath.Text))
+            {
+                Properties.Settings.Default.Path117 = txt117Path.Text;
+                Properties.Settings.Default.Path473 = txt473Path.Text;
+                Properties.Settings.Default.PathGAPS = txtGapsPath.Text;
                 Properties.Settings.Default.Save();
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
@@ -55,43 +58,6 @@ namespace SaveWorkbook
             this.Close();
         }
         #endregion
-
-        private bool SetPath(out string path)
-        {
-            FolderBrowserDialog fd = new FolderBrowserDialog();
-            bool result;
-
-            if (fd.ShowDialog() == DialogResult.OK)
-            {
-                path = fd.SelectedPath;
-                if (path.Substring(path.Length - 1) != "\\")
-                    path += "\\";
-
-                result = true;
-            }
-            else
-            {
-                path = String.Empty;
-                result = false;
-            }
-
-            fd.Dispose();
-
-            return result;
-        }
-
-        private bool IsValidPath(string path)
-        {
-            Regex rxFilePath = new Regex(@"^(?:[A-Za-z]\:\\|\\\\[\w.]+\\)(?:[^\\ ][\w!@#$%^&()_+;'\.,  .]*\\)+$");
-            return rxFilePath.IsMatch(path);
-        }
-
-        private void frmSettings_Load(object sender, EventArgs e)
-        {
-            txt117Path.Text = Properties.Settings.Default.Path117;
-            txt473Path.Text = Properties.Settings.Default.Path473;
-            txtGapsPath.Text = Properties.Settings.Default.PathGAPS;
-        }
 
         #region LostFocus events
         private void txtGapsPath_Leave(object sender, EventArgs e)
@@ -127,5 +93,42 @@ namespace SaveWorkbook
                 txt473Path.BackColor = Color.LightPink;
         }
         #endregion
+
+        private void frmSettings_Load(object sender, EventArgs e)
+        {
+            txt117Path.Text = Properties.Settings.Default.Path117;
+            txt473Path.Text = Properties.Settings.Default.Path473;
+            txtGapsPath.Text = Properties.Settings.Default.PathGAPS;
+        }
+
+        private bool SetPath(out string path)
+        {
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            bool result;
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                path = fd.SelectedPath;
+                if (path.Substring(path.Length - 1) != "\\")
+                    path += "\\";
+
+                result = true;
+            }
+            else
+            {
+                path = String.Empty;
+                result = false;
+            }
+
+            fd.Dispose();
+
+            return result;
+        }
+
+        private bool IsValidPath(string path)
+        {
+            Regex rxFilePath = new Regex(@"^(?:[A-Za-z]\:\\|\\\\[\w.]+\\)(?:[^\\ ][\w!@#$%^&()_+;'\.,  .]*\\)+$");
+            return rxFilePath.IsMatch(path);
+        }
     }
 }
